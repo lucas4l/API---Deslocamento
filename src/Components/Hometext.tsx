0002dbd463a8ee1daa54b15d0017637ec339c4c7
lucas4l <barbosa-lucas2@hotmaillcom>
2023-06-30 17:44:48 -0300

    mudando texto inicial

--- src/Components/Hometext.tsx
+++ src/Components/Hometext.tsx
@@ -18,18 +18,12 @@ export default function HomeText() {
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
       <DrawerHeader />
       <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-        non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-        tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non
-        tellus. Convallis convallis tellus id interdum velit laoreet id donec
-        ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
-        suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
-        quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet
-        proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
-        tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
-        varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
-        Lorem donec massa sapien faucibus et molestie ac.
+        Este é um projeto criado utilizando Material UI, Next, React e
+        Typescript. As rotas GET, PUT, DELETE, POST são feitas de forma dinâmica
+        de forma a facilitar a manutenção do projeto, futuras atualizações ao
+        projeto serão feitas para melhoria e customização.
+        <br />
+        Projeto feito por Lucas Barbosa de Lima
       </Typography>
     </Box>
   )
